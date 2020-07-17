38d50e8ce21c3f8c87542240214eb3ee68ffcd5e
Eric C <erix2205@gmail.com>
2020-07-17 08:07:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function viewCart() {
     return "Your shopping cart is empty."
   }
   else if (cart.length = 1) {
-    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+    return `In your cart, you have ${cart[0].name} at $${cart[0].itemPrice}.`
       }
   else if (cart.length = 2) {
     return `In your cart, you have ${cart[0].name} at $${cart[0].itemPrice} and ${cart[1].name} at $${cart[1].itemPrice}.`

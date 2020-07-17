aa73465ccfcc1ffb7d66c3802be0529de8f631f0
Eric C <erix2205@gmail.com>
2020-07-17 09:24:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -63,7 +63,8 @@ function placeOrder(cardNumber) {
     return 'Sorry, we don\'t have a credit card on file for you.'
   }
   else {
+    var charge = total()
     cart = []
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${charge} which will be charged to the card ${cardNumber}.`
   }
 }

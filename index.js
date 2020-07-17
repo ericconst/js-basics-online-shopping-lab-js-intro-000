fd1709c21575dd86b91909e623699bf84b5b168f
Eric C <erix2205@gmail.com>
2020-07-17 09:09:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -48,13 +48,9 @@ function total() {
 }
 
 function removeFromCart(item) {
-  var found = false
-  var element
   for (let i = 0; i < cart.length; i++){
-    element = cart.slice(i, i + 1);
-    if (`${item}`==`${element.itemName}`) {
+    if (item === cart[i].itemName)
       cart.splice(i, 1);
-      found = true
     }
   }
   if (found = false) {
